f1bb69e308add83f2e1aa208d51cf2764c048d54
Connor McKelvey <connormmckelvey@gmail.com>
2026-04-14 21:24:39 -0500

    patches

--- worker/src/lib/github.js
+++ worker/src/lib/github.js
@@ -30,6 +30,7 @@ const githubRequest = async (env, path, options = {}) => {
       Authorization: `Bearer ${token}`,
       Accept: 'application/vnd.github+json',
       'X-GitHub-Api-Version': '2022-11-28',
+      'User-Agent': 'portfolio-admin-api',
       ...(options.headers || {})
     }
   });
